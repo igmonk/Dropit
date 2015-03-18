7c7751f7599bf5bcc6636a74e2f1917e4c526b51
Igor Moiseyenko <imoiseyenko93@gmail.com>
2015-03-18 13:20:51 +0300

    Initial Commit

---
+++ Dropit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dropit
+//
+//  Created by Igor on 3/18/15.
+//  Copyright (c) 2015 Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
